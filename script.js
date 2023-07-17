0fb9b88d880a5b773493981e0fb745383aa5de5f
Mohammad <80725665+MohammadRostamiorg@users.noreply.github.com>
2023-07-17 14:59:29 +0330

    fix some bugs

--- script.js
+++ script.js
@@ -3,20 +3,7 @@ const btnRight = document.querySelector('.btn-right')
 const slider = document.querySelector('#slider')
 const slides = document.querySelectorAll('.slideItem')
 const slidesCount = slides.length;
-const circles = document.querySelector('.circles')
 let currentSlide = 0;
-
-// function showCircle(){
-//     circles.innerHTML = "";
-//     for(i=0;i <= slidesCount;i++){
-//         let circle = document.createElement('circle');
-//         circle.classList.add('circle')
-//         circle.dataset.slideId = i;
-//         circles.appendChild(circle);
-        
-//     }
-//     document.querySelectorAll('.circle')[currentSlide].classList.add('active')
-// }
 
 function slideLeft(){
     if(currentSlide == 0){
@@ -44,21 +31,7 @@ function slideRight(){
     slides[currentSlide].classList.add('active')
 
 }
-
 btnLeft.addEventListener('click',slideLeft)
 btnRight.addEventListener('click',slideRight)
 
-
 setInterval(slideLeft,5000)
-// showCircle()
-
-
-// document.querySelectorAll('.circle').forEach(element => {
-//     element.addEventListener('click',(e) => {
-//         console.log(e.target.dataset.slideId);
-//     })
-// });
-
-// document.querySelector('.circle').addEventListener('click', e => {
-//     console.log(e.target);
-// })
